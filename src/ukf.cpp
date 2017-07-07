9e3f62ab5824c13584a71e2bd8548e92908a3b98
adelman <samuel.adelman@gmail.com>
2017-07-07 19:50:57 -0400

    update lidar

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -277,9 +277,74 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
   */
   cout << "LASER/update: " << meas_package.raw_measurements_ << endl;
 
+  //set measurement dimension, radar can measure px, py
+  int n_z = 2;
+  //create matrix for sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, 15);
 
-  cout << "X: " << endl << x_ << endl;
+  //measurement covariance matrix S
+  MatrixXd S = MatrixXd(n_z, n_z);
 
+  //transform sigma points into measurement space
+  for (int i = 0; i < 15; i++) {
+    // extract values for better readibility
+    double p_x = Xsig_pred_(0, i);
+    double p_y = Xsig_pred_(1, i);
+
+    // measurement model
+    Zsig(0, i) = p_x;
+    Zsig(1, i) = p_y;
+  }
+  //calculate mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
+  z_pred.fill(0.0);
+  for (int i = 0; i < 15; i++) {
+    z_pred = z_pred + weights_(i) * Zsig.col(i);
+  }
+  cout << "z_pred: " << endl << z_pred << endl;
+
+
+  //calculate measurement covariance matrix S
+  S.fill(0.0);
+  for (int i = 0; i < 15; i++) {
+    VectorXd z_diff = Zsig.col(i) - z_pred;
+
+    S = S + weights_(i) * z_diff * z_diff.transpose();
+
+  }
+  //add measurement noise covariance matrix
+  MatrixXd R = MatrixXd(n_z, n_z);
+  R << std_laspx_ * std_laspx_, 0,
+          0, std_laspy_ * std_laspy_;
+  S = S + R;
+
+  //This is the first place the measurement value is actually used!
+  //create example vector for incoming radar measurement
+  VectorXd z = VectorXd(n_z);
+  z <<
+    meas_package.raw_measurements_(0),   //x
+          meas_package.raw_measurements_(1);   //y
+
+  //create matrix for cross correlation Tc
+  MatrixXd Tc = MatrixXd(n_x_, n_z);
+
+  Tc.fill(0.0);
+  //calculate cross correlation matrix
+  for (int i = 0; i < 15; i++) {
+    VectorXd x_diff = Xsig_pred_.col(i) - x_;
+    VectorXd z_diff = Zsig.col(i) - z_pred;
+
+    Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
+  }
+
+  //calculate Kalman gain K;
+  MatrixXd K = Tc * S.inverse();
+
+  //update state mean and covariance matrix
+  VectorXd z_diff = z - z_pred;
+
+  x_ = x_ + K * z_diff;
+  P_ = P_ - K * S * K.transpose();
 }
 
 /**
